f8c3cd439a9ae222a0cf3e1f999b0a7a392b30ca
AndrewLivancov <gilamrot@gmail.com>
2021-10-16 10:14:47 +0300

    add 1st sample c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t;
+    t =  25;
+    if(t > 10)
+    {   printf("www");
+            if (t > 20)
+        printf("123");
+
+    return 2;
+    }
+        else
+        { printf("ggg");
+          return 7;
+        }
+
+}
