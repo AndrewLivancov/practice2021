def680f97547a2f34b183cc9427e492b3ba194c8
AndrewLivancov <gilamrot@gmail.com>
2022-04-17 14:28:41 +0300

    f

--- 16_1.c
+++ 16_1.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
     float f = 2.5;
-    printf("%f", f - (int)f);
+    printf("%f\n", f - (int)f  );
+    printf("hey how are you?\n");
+    printf("I'am fine.\n");
+    printf("Let's do some code\n");
+    printf("my computer do some crap...\n");
+
     return 0;
 }
